b8bb148a7e0d9362f857afda08fcd344844f113b
apcs <apcs@mvhs-imac31u528.dcsdk12.org>
2013-09-03 08:07:08 -0600

    for loops working

--- loops.java
+++ loops.java
@@ -5,9 +5,15 @@ public class loops {
 
 		int x = 1;
 
-		while (x<101) {
-			System.out.println(x);
-			x = x+1;
+		//while (x<101) {
+		//	System.out.println(x);
+		//	x = x+1;
+		//}
+
+
+
+		for (int i=1; i <= 100; i++) {
+			System.out.println(i);
 		}
 
 	}
